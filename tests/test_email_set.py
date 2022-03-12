14ba15789844ea5130ad6bfedc3d5d12d13bcb29
ColbyRehn <96672574+ColbyRehn@users.noreply.github.com>
2022-03-12 14:32:46 +1100

    Added clear fixture and doc string.

--- tests/test_email_set.py
+++ tests/test_email_set.py
@@ -2,12 +2,11 @@ import json
 import requests
 import pytest
 
-def test_email_set():
+def test_email_set(clear):
     '''
     Basic test for email_set()
-    User provides their email to be used as the receiving address
+    User provides their email to be used as the receiving address. Expect token to be generated correctly.
     '''
-    requests.delete(f"{url}/clear/v1")
     response = requests.post(f"{url}/email/set", json = {
     "email": "user1@gmail.com",
     "password": "Strongpword1"
@@ -16,12 +15,11 @@ def test_email_set():
     encode_token = generate_token(1, 1)
     assert json.loads(response.text) == {'token': encode_token}
 
-def test_invalid_email_set():
+def test_invalid_email_set(clear):
     '''
     Exception test for email_set()
-    User provides an invalid email
+    User provides an invalid email. Expect InputError.
     '''
-    requests.delete(f"{url}/clear/v1")
     response = requests.post(f"{url}/email/set", json = {
     "email": "user1.com",
     "password": "Strongpword1"
@@ -29,12 +27,11 @@ def test_invalid_email_set():
 
     assert response.status_code == 400
 
-def test_email_set_two():
+def test_email_set_two(clear):
     '''
     Exception test for email_set()
-    User provides an email which is already in use
+    User provides an email which is already in use. Expect InputError.
     '''
-    requests.delete(f"{url}/clear/v1")
     response = requests.post(f"{url}/email/set", json = {
     "email": "user1@gmail.com",
     "password": "Strongpword1"
