48d0511fc78b14d9a980f7a166f23fad9c5c2eae
Kian-Blakley <94435771+Kian-Blakley@users.noreply.github.com>
2022-03-12 14:04:36 +1100

    Revert "Added colby's database.py, changed clear.py to call his functions"
    This reverts commit 13268f31d575270a258fe4ef1382305565a65a30.

--- src/clear.py
+++ src/clear.py
@@ -1,7 +1,17 @@
-from database import Database
+import sqlite3
 
-# Clears all data from SQLite database
-
-
+# Clears all data from the user table in SQLite database
 def clear():
-    Database.drop_tables()
+    try:
+        connection = sqlite3.connect('Db.db')
+        cursor = connection.cursor()
+        query = "DELETE FROM Users"
+        cursor.execute(query)
+        connection.commit()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if connection:
+            connection.close()
