6d47c76fb1149aadeb7b29136f235fcc64defa5c
Elise <z5312858@ad.unsw.edu.au>
2022-03-13 15:49:09 +1100

    added implementation for login function to user.py

--- src/user.py
+++ src/user.py
@@ -10,7 +10,26 @@ def user_login(email_address, password):
     '''
     some description
     '''
-    return {'token' : 'Change this'}
+    validate = 0
+    all_email = [user.email for user in Database.get_table('Register')]
+    for email in all_email: 
+        if email == email_address:
+            validate = 1
+            match = email
+            break
+    # how do we get user_id from database?
+    newToken = jwt.encode({'user_id': user_id, 'session_id': session_id}, SECRET_KEY, algorithm='HS256')
+
+    for user in Database.get_table('Register'):
+        if user.email == email_address:
+            if user.password == password:
+                validate = 1
+                Database.update('Login', token_data['user_id'], {'email' : email})
+    
+    if validate == 0:
+        raise InputError(description='Email not in the list or password is incorrect')
+    
+    return {'token' : 'newToken'}
 
 def user_logout(token):
     '''
