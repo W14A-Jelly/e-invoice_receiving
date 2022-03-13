fb78d5d61d89f0fec39c25b46eafea13df21d3d6
Elise <z5312858@ad.unsw.edu.au>
2022-03-13 16:36:50 +1100

    fixed token generation for login in user.py

--- src/user.py
+++ src/user.py
@@ -11,30 +11,33 @@ def user_login(email_address, password):
     some description
     '''
     validate = 0
-    all_email = [user.email for user in Database.get_table('Register')]
+    all_email = [user.email for user in Database.get_table('Login')]
     for email in all_email: 
         if email == email_address:
             validate = 1
-            match = email
             break
-    # how do we get user_id from database?
-    newToken = jwt.encode({'user_id': user_id, 'session_id': session_id}, SECRET_KEY, algorithm='HS256')
-
-    for user in Database.get_table('Register'):
+ 
+    for user in Database.get_table('Login'):
         if user.email == email_address:
             if user.password == password:
                 validate = 1
-                Database.update('Login', token_data['user_id'], {'email' : email})
+                session = user.session_id
+                userID = user.user_id
+                Database.update('Login', userID, {'email' : email})
+                break
     
     if validate == 0:
         raise InputError(description='Email not in the list or password is incorrect')
-    
-    return {'token' : 'newToken'}
+
+    newToken = jwt.encode({'user_id': userID, 'session_id': session}, SECRET_KEY, algorithm='HS256')
+    return {'token' : newToken}
 
 def user_logout(token):
     '''
     some description
     '''
+    tokendecode = 
+
     return {}
 
 def user_update_email(token, email):
