40ee5f3f027d417b0fb8741653dc9ae07a3b68c5
Kian-Blakley <94435771+Kian-Blakley@users.noreply.github.com>
2022-03-29 14:28:56 +1100

    cleared reports folder, removed unecessary import from report.py

--- src/report.py
+++ src/report.py
@@ -9,7 +9,6 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from tkinter import N
 
 # Helper functions for generating a communication report
 
